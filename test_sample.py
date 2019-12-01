37582c30a49ec2eea86f09c30ed3fe5ebada07b8
liangliang <liangliang1987429@163.com>
2019-12-01 12:08:35 +0800

    update

--- test_sample.py
+++ test_sample.py
@@ -1,6 +1,17 @@
 from nose.tools import assert_equal
 from funcs import *
+import pytest
 
 
 def test_func1():
     assert_equal(func1(3), 4)
+
+
+@pytest.fixture
+def chrome_options(chrome_options):
+    chrome_options.add_argument('--headless')
+    return chrome_options
+
+
+def test_open_web(selenium, chrome_options):
+    selenium.get('http://www.baidu.com')
