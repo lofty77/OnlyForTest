ae4e776b5c9c3a1f296f98ff64d658b746cc0189
liangliang <liangliang1987429@163.com>
2019-12-02 20:02:33 +0800

    u2

--- test_sample.py
+++ test_sample.py
@@ -1,17 +1,31 @@
 from nose.tools import assert_equal
 from funcs import *
 import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import TimeoutException
 
 
 def test_func1():
     assert_equal(func1(3), 4)
 
 
-@pytest.fixture
-def chrome_options(chrome_options):
+def test_assert_title_of_homepage():
+    chrome_options = Options()
     chrome_options.add_argument('--headless')
-    return chrome_options
+    browser = webdriver.Chrome(options=chrome_options)
+    browser.get("https://www.engagespark.com/")
+    assert browser.title == "Home - Send and Receive Automated Call and SMS Text Campaigns."
 
 
-def test_open_web(selenium, chrome_options):
-    selenium.get('http://www.baidu.com')
+# @pytest.fixture
+# def chrome_options(chrome_options):
+#     chrome_options.add_argument('--headless')
+#     return chrome_options
+
+
+# def test_open_web(selenium, chrome_options):
+#     selenium.get('http://www.baidu.com')
